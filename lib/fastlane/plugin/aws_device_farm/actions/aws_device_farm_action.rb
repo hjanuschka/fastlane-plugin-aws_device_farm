80fe650b49b20d221cfbf9be3252b56e490127c5
LokeshReddy <lokeshreddyp@users.noreply.github.com>
2020-06-05 10:26:23 -0500

    update type
    update type

--- lib/fastlane/plugin/aws_device_farm/actions/aws_device_farm_action.rb
+++ lib/fastlane/plugin/aws_device_farm/actions/aws_device_farm_action.rb
@@ -370,7 +370,7 @@ module Fastlane
         @client.create_upload({
           project_arn:  project.arn,
           name:         File.basename(path),
-          content_type: 'application/octet-stream',
+#           content_type: 'application/octet-stream',
           type:         type
         }).upload
       end
@@ -379,7 +379,7 @@ module Fastlane
         url = URI.parse(upload.url)
         contents = File.open(path, 'rb').read
         Net::HTTP.new(url.host).start do |http|
-          http.send_request("PUT", url.request_uri, contents, { 'content-type' => 'application/octet-stream', 'Authorization' => 'AWS4-HMAC-SHA256' })
+          http.send_request("PUT", url.request_uri, contents, { 'content-type' => 'application/x-www-form-urlencoded' })
         end
       end
 
