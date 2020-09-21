84138f3cb0259d94798f8feb87f4d386c2b4f730
LokeshReddy <lokeshreddyp@users.noreply.github.com>
2020-09-21 13:29:05 -0500

    Add additional logging to get errors
    Add additional logging to get errors

--- lib/fastlane/plugin/aws_device_farm/actions/aws_device_farm_action.rb
+++ lib/fastlane/plugin/aws_device_farm/actions/aws_device_farm_action.rb
@@ -458,7 +458,7 @@ module Fastlane
           end
           run = fetch_run_status run
         end
-        UI.message "The run ended with result #{run.result}."
+        UI.message "The run ended with result #{run}."
         UI.important "Minutes Counted: #{run.device_minutes.total}"
 
         UI.verbose "RUN ARN: #{run.arn}."
