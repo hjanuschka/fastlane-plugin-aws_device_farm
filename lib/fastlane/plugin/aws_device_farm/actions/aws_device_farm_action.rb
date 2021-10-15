30d47fb51f2111a1cd0faf1181ef6b2ae833f440
Sebastian Harner <s.harner@web.de>
2021-10-15 20:16:21 +0200

    Fix breaking change introduced with network_profile_arn (#50)
    * Make network_profile_arn optional in order to avoid breaking changes
    
    * Update Readme.md to align with possible parameters in action
    
    Co-authored-by: Sebastian Harner <sebastian.harner@novatec-gmbh.de>

--- lib/fastlane/plugin/aws_device_farm/actions/aws_device_farm_action.rb
+++ lib/fastlane/plugin/aws_device_farm/actions/aws_device_farm_action.rb
@@ -230,8 +230,7 @@ module Fastlane
             key:         :network_profile_arn,
             env_name:    'FL_AWS_DEVICE_FARM_NETWORK_PROFILE_ARN',
             description: 'Network profile arn you want to use for running the applications',
-            default_value: nil,
-            optional:    false,
+            optional:    true,
             is_string:   true
           ),
           FastlaneCore::ConfigItem.new(
